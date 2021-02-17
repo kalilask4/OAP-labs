cd699d5340dd65f1b09f80e55285de96c4b22a05
kalilask4 <kalilask4@gmail.com>
2021-02-17 16:14:38 +0300

    added f. for array input

--- OAP-labs/l5.cpp
+++ OAP-labs/l5.cpp
@@ -5,7 +5,9 @@ using namespace std;
 
 int rows, cols;
 
-void main() {
+void enter_array(int** arr, int rows, int cols);
+
+int main() {
 	setlocale(LC_CTYPE, "RUSSIAN");
 	do {
 		cout << "¬ведите количество строк:  " << endl;
@@ -17,4 +19,21 @@ void main() {
 	} while (cols <= 0);
 	int** arr;
 	arr = new int* [rows];
+	for (int i = 0; i < rows; i++)
+	{
+		arr[i] = new int[cols];
+	}
+	cout << "¬вод массива:" << endl;
+	enter_array(arr, rows, cols);
+	return 0;
+}
+
+void enter_array(int** arr, int rows, int cols) {
+	for (int i = 0; i < rows; i++) {
+		cout << "¬ведите строку номер:" << "  " << i + 1 << endl;
+		for (int j = 0; j < cols; j++)
+		{
+			cin >> *(*(arr + i) + j);
+		}
+	}
 }
