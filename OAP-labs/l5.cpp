fd94dcffbc6c4bfaafa776630f601ae040dd31b1
kalilask4 <kalilask4@gmail.com>
2021-02-17 16:18:12 +0300

    added array out

--- OAP-labs/l5.cpp
+++ OAP-labs/l5.cpp
@@ -6,6 +6,7 @@ using namespace std;
 int rows, cols;
 
 void array_input(int** arr, int rows, int cols);
+void out_array(int** arr, int rows, int cols);
 
 int main() {
 	setlocale(LC_CTYPE, "RUSSIAN");
@@ -25,6 +26,10 @@ int main() {
 	}
 	cout << "¬вод массива:" << endl;
 	array_input(arr, rows, cols);
+	out_array(arr, rows, cols);
+
+
+
 	return 0;
 }
 
@@ -35,5 +40,16 @@ void array_input(int** arr, int rows, int cols) {
 		{
 			cin >> *(*(arr + i) + j);
 		}
+	}
+}
+
+void out_array(int** arr, int rows, int cols) {
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < cols; j++)
+		{
+			cout << *(*(arr + i) + j) << "\t";
+		}
+		cout << endl;
 	}
 }
